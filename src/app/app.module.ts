a0dca9f76003ceefda4dd2ab201168c5f5870ff9
nachaphonchoto <nachaphonchoto@gmail.com>
2022-05-02 15:26:43 +0700

    login page and project-list page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,22 @@ import { MatListModule} from '@angular/material/list'
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+
+
 import { SpentTimeComponent } from './pages/spent-time/spent-time.component';
 import { ProjectListComponent } from './pages/project-list/project-list.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+
+
 @NgModule({
-  declarations: [AppComponent, NotFoundComponent, DashboardComponent, ProjectListComponent, SpentTimeComponent],
+  declarations: [AppComponent, NotFoundComponent, DashboardComponent, ProjectListComponent, SpentTimeComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -27,6 +37,11 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
     MatIconModule,
     MatDividerModule,
     MatListModule,
+    FormsModule,
+    MatInputModule,
+    MatTableModule,
+    MatMenuModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
