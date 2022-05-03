6e58a3895b4647acae41fe4bb3d5fa2c1030d80c
Tanawat Palaboon <ten07@hotmail.co.th>
2022-05-03 17:53:09 +0700

    fix cors by using chrome

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  message = '';
 
-  constructor() { }
+  constructor(
+  private http: HttpClient,
+  private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.http.get('http://localhost:8000/api/user')
+    .subscribe({
+      next: (res: any) => {
+        this.message = `Hi ${res.progile}`;
+      },
+      error: () => {
+        this.router.navigate(['/login']);
+      }
+    });
   }
 
 }
