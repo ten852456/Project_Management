78f02410dc1b706adcad202614b782c7d3c8a152
can <laksika.0175705@gmail.com>
2022-05-04 14:55:37 +0700

    spentime page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
-import { MatListModule} from '@angular/material/list'
+import { MatListModule } from '@angular/material/list'
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatTableModule } from '@angular/material/table';
 
 import { SpentTimeComponent } from './pages/spent-time/spent-time.component';
 import { ProjectListComponent } from './pages/project-list/project-list.component';
@@ -30,6 +35,11 @@ import { LoginComponent } from './pages/login/login.component';
     MatIconModule,
     MatDividerModule,
     MatListModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatButtonToggleModule,
+    MatTableModule,
     ReactiveFormsModule,
     HttpClientModule,
   ],
@@ -42,4 +52,4 @@ import { LoginComponent } from './pages/login/login.component';
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
