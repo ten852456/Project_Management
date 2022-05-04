84d54faf4037d070e0ba58eb268e8e227621b7fb
Tanawat Palaboon <ten07@hotmail.co.th>
2022-05-04 17:28:37 +0700

    add post to backend

--- src/app/pages/project-list/project-list.component.ts
+++ src/app/pages/project-list/project-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-project-list',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-list.component.scss']
 })
 export class ProjectListComponent implements OnInit {
+  message = '';
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(    private formBuilder: FormBuilder,
+        private http: HttpClient,
+    ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      startDate: '',
+      title: '',
+      finishDate: '',
+      sprint: {id : '1'},
+      estimatedDays: '',
+      description:''
+    });
+  }
+
+  addProject() {
+    this.http.post('http://localhost:8080/api/task', this.form.getRawValue(), {withCredentials: true})
+      .subscribe((res: any) => {
+        this.message = JSON.stringify(res);
+      });
   }
   
 }
