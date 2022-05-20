42974bf867b1f1e2858d49e0e2af1f691021b0e0
NutchaDan <nutcha.siriwut@gmail.com>
2022-05-20 17:49:17 +0700

    error displayName

--- src/app/pages/member/member-dialog/member-dialog.component.ts
+++ src/app/pages/member/member-dialog/member-dialog.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
-@Component({
+import { HttpClient } from '@angular/common/http';@Component({
   selector: 'app-member-dialog',
   templateUrl: './member-dialog.component.html',
   styleUrls: ['./member-dialog.component.scss']
 })
 
 export class MemberDialogComponent implements OnInit {
+
+  // form = new FormGroup({
+  //   username: new FormControl(),
+  //   roles: new FormControl(),
+  //   profile: new FormGroup({
+  //     displayName: new FormControl(),
+  //   })
+  // })
 
   form!: FormGroup;
   selectRole: string | undefined;
@@ -19,16 +26,41 @@ export class MemberDialogComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      displayName: '',
-      username: '',
-    });
+    // this.form = this.formBuilder.group({
+    //   username: '',
+    //   profile: ({
+    //     displayName: '',
+    //   })
+    // });
+    this.form = new FormGroup({
+      username: new FormControl(),
+      roles: new FormControl(),
+      profile: new FormGroup({
+        displayName: new FormControl(),
+      })
+    })
   }
 
   addMember() {
+    // this.form = this.getForm(data);
     this.http.post<any>(this.userListUrl, this.form.getRawValue(), {withCredentials: true})
         .subscribe((res: any) => {});
-    window.location.reload();
+    // window.location.reload();
   }
 
+  // getForm(data:any) {
+  //   data=data || {  username:null,
+  //                   roles: null,
+  //                   profile:{
+  //                     displayName:null,
+  //                   }
+  //                }
+  //   return new FormGroup({
+  //       username: new FormControl(data.username),
+  //       roles: new FormControl(data.roles),
+  //       profile: new FormGroup({
+  //           displayName: new FormControl(data.profile.displayName),
+  //        })
+  //    })
+  // }
 }
