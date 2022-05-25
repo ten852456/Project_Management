ead6fdcf6557a551152301684c23d5373ec72802
NutchaDan <nutcha.siriwut@gmail.com>
2022-05-25 14:25:59 +0700

    error role and displayName

--- src/app/pages/member/member-dialog/member-dialog.component.ts
+++ src/app/pages/member/member-dialog/member-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';@Component({
   selector: 'app-member-dialog',
@@ -9,17 +9,18 @@ import { HttpClient } from '@angular/common/http';@Component({
 
 export class MemberDialogComponent implements OnInit {
 
-  // form = new FormGroup({
-  //   username: new FormControl(),
-  //   roles: new FormControl(),
-  //   profile: new FormGroup({
-  //     displayName: new FormControl(),
-  //   })
-  // })
+  form = new FormGroup({
+    username: new FormControl(),
+    roles: new FormControl('ROLE_USER', Validators.required),
+    profile: new FormGroup({
+      displayName: new FormControl(),
+    })
+  })
 
-  form!: FormGroup;
-  selectRole: string | undefined;
-  roles: string[] = ['System Admin', 'User'];
+  // form!: FormGroup;
+  // selectRole: string | undefined;
+  // roles: string[] = ['System Admin', 'User'];
+  selected = '?roles=ROLE_USER';
 
   private userListUrl = environment.userListUrl;
 
@@ -32,35 +33,36 @@ export class MemberDialogComponent implements OnInit {
     //     displayName: '',
     //   })
     // });
-    this.form = new FormGroup({
-      username: new FormControl(),
-      roles: new FormControl(),
-      profile: new FormGroup({
-        displayName: new FormControl(),
-      })
-    })
+
+    // this.form = new FormGroup({
+    //   username: new FormControl(),
+    //   roles: new FormControl(),
+    //   profile: new FormGroup({
+    //     displayName: new FormControl(),
+    //   })
+    // })
   }
 
-  addMember() {
-    // this.form = this.getForm(data);
+  addMember(data: any) {
+    this.form = this.getForm(data);
     this.http.post<any>(this.userListUrl, this.form.getRawValue(), {withCredentials: true})
         .subscribe((res: any) => {});
     // window.location.reload();
   }
 
-  // getForm(data:any) {
-  //   data=data || {  username:null,
-  //                   roles: null,
-  //                   profile:{
-  //                     displayName:null,
-  //                   }
-  //                }
-  //   return new FormGroup({
-  //       username: new FormControl(data.username),
-  //       roles: new FormControl(data.roles),
-  //       profile: new FormGroup({
-  //           displayName: new FormControl(data.profile.displayName),
-  //        })
-  //    })
-  // }
+  getForm(data: any) {
+    data=data || {  username:null,
+                    roles: null,
+                    profile:{
+                      displayName:null,
+                    }
+                 }
+    return new FormGroup({
+        username: new FormControl(data.username, Validators.required),
+        roles: new FormControl(data.roles, Validators.required),
+        profile: new FormGroup({
+            displayName: new FormControl(data.profile.displayName),
+         })
+     })
+  }
 }
